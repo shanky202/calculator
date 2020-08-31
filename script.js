08557da243ee265275e72fefa9236cc976c23037
shanky202 <shankypachore@gmail.com>
2020-08-31 13:37:06 +0000

    added operator functionality and complete the project

--- script.js
+++ script.js
@@ -8,22 +8,53 @@ let operatorValue = '';
 let awaitingNextValue = false;
 
 function sendNumberValue(number) {
-    // if current display is 0, replace it, if not add number
-    const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = (displayValue === '0') ? number : displayValue + number;
+    // Replace current value if first value is entered
+    if (awaitingNextValue) {
+        calculatorDisplay.textContent =  number;
+        awaitingNextValue = false;
+    } else {
+        // if current value is zero, replace it, if not add number
+        const displayValue = calculatorDisplay.textContent;
+        calculatorDisplay.textContent = (displayValue === '0') ? number : displayValue + number;
+    }
+    
+}
+
+// Calculate First and second values depending on operator
+const calculate = {
+    '/' : (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*' : (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+' : (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-' : (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=' : (firstNumber, secondNumber) => secondNumber,
 }
 
 
 function useOperator(operator) {
     const currentValue = Number(calculatorDisplay.textContent);
+    // Prevent Multiple Operator
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    };
     // Assign FirstValue if no value
     if (!firstValue) {
         firstValue = currentValue;
+    } else {
+        const calculation = calculate[operatorValue](firstValue,currentValue);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
     }
+
+    // ready to store next value
+    awaitingNextValue = true;
     operatorValue = operator;
+    
 }
 
 function addDecimal() {
+    // if operator operator pressed, dont add decimal
+    if (awaitingNextValue) return;
     // if no decimal add one
     if (!calculatorDisplay.textContent.includes('.')) {
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
@@ -44,10 +75,10 @@ inputBtns.forEach((inputBtn) => {
 
 // Reset display
 function resetAll() {
+    firstValue = 0;
+    operatorValue = ''; 
+    awaitingNextValue = false;
     calculatorDisplay.textContent = '0';
-     firstValue = 0;
-     operatorValue = ''; 
-     awaitingNextValue = false;
 }
 
 clearBtn.addEventListener('click', resetAll);
